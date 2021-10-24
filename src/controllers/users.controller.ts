55203a1abcb128732b98c540f454b4b43fe9b859
Ian Rivas <rivas.ian.javier@gmail.com>
2021-10-24 02:34:51 -0300

    add users routes and controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,9 @@
 import {Request, Response} from 'express';
 import {pool} from '../database';
+import { QueryResult } from 'pg';
 
 export const sayHi = (req:Request,res:Response) => {
-    res.json({hola:'mundo'})
+    res.send('HOLA MUNDO');
 }
 
 export const createTables = async (req:Request,res:Response) => {
@@ -17,5 +18,72 @@ export const createTables = async (req:Request,res:Response) => {
         res.send('HOLA MUNDO Docker again');
     } catch (e) {
         console.log(e)
+    }
+}
+
+export const getUsers = async (req:Request,res:Response): Promise<Response> => {
+    try {
+        const {rows}:QueryResult = await pool.query('SELECT * FROM users');
+        return res.status(200).json(rows);
+    } catch (e) {
+        console.log(e);
+        return res.send('Internal Error');
+    }
+}
+export const getUserById = async (req:Request,res:Response): Promise<Response> => {
+    try {
+        const id= req.params.id;
+        const {rows} = await pool.query('SELECT * FROM users WHERE id = $1',[id]);
+        return res.status(200).json(rows);
+    } catch (e) {
+        console.log(e);
+        return res.send('Internal Error');
+    }
+}
+
+
+export const createUsers = async (req:Request,res:Response): Promise<Response> => {
+    try {
+        const {name,email} = req.body;
+        await pool.query('INSERT INTO users(name,email) VALUES($1,$2)',[name,email]);
+        return res.status(200).json({
+            message: 'User created successfully',
+            user: {
+                name,
+                email
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        return res.send('Internal Error');
+    }
+}
+
+export const editUsers = async (req:Request,res:Response): Promise<Response> => {
+    try {
+        const id = Number(req.params.id);
+        const {name,email} = req.body;
+        await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3',[name,email,id]);
+        return res.status(200).json({
+            message: 'User Updated successfully',
+            user: {
+                name,
+                email
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        return res.send('Internal Error');
+    }
+}
+
+export const deleteUsers = async (req:Request,res:Response): Promise<Response> => {
+    try {
+        const id = Number(req.params.id);
+        await pool.query('DELETE FROM users WHERE id = $1',[id]);
+        return res.status(200).send('User deleted Successfully');
+    } catch (e) {
+        console.log(e);
+        return res.send('Internal Error');
     }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import {Router} from 'express';
 const router = Router();
-import {createTables, sayHi} from '../controllers/users.controller';
+import {createTables, sayHi, getUsers, createUsers, getUserById, editUsers, deleteUsers} from '../controllers/users.controller';
 
 
 router.get('/',sayHi);
 router.get('/createTableUser',createTables)
+router.get('/users',getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users',createUsers);
+router.put('/users/:id',editUsers);
+router.delete('/users/:id', deleteUsers);
 
 
 export default router;
